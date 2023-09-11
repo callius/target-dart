29efe2bf321e4e8b32dc79c2d37deede6e2006af
callius <36176320+callius@users.noreply.github.com>
2023-09-11 15:55:29 -0700

    Fix: Pass Builder Options
    - Pass `BuilderOptions` to `PartBuilder`

--- packages/target_annotation_processor/lib/target_annotation_processor.dart
+++ packages/target_annotation_processor/lib/target_annotation_processor.dart
@@ -3,5 +3,5 @@ import 'package:source_gen/source_gen.dart';
 import 'package:target_annotation_processor/src/model_generator.dart';
 
 Builder modelBuilder(BuilderOptions options) {
-  return PartBuilder([ModelGenerator()], '.core.dart');
+  return PartBuilder([ModelGenerator()], '.core.dart', options: options);
 }
